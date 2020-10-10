ae1e8e5e7a669ef3cdafb6089471511ca35e5ba9
Bharat kumar <70039924+tBharat7@users.noreply.github.com>
2020-10-10 23:10:34 +0530

    Create alcohol_detection_sensor(mq3).cpp
    mq3

---
+++ basics/alcohol_detection_sensor(mq3).cpp
@@ -0,0 +1,26 @@
+int DOUTpin=10;
+int LEDpin=13;
+
+void setup() {
+pinMode(DOUTpin, INPUT);
+pinMode(LEDpin, OUTPUT);
+
+}
+
+void loop() {
+alcohol
+delay(3000);
+}
+
+void alcohol() 
+{
+value=digitalRead(DOUTpin) ;
+Serial. print("value:") ;
+delay(1000) ;
+if(value==HIGH) {
+digitalWrite(LEDpin, HIGH) ;
+}
+else{
+digitalWrite(LEDpin, LOW) ;
+}
+}
